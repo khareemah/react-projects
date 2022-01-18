a1606bffb5b8bc0dc2afa2ee67517e5c7b3adbd1
kareemah <ajimobikareemah@gmail.com>
2022-01-18 15:21:35 +0100

    tours component

--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({ tours, removeTour }) => {
+  return (
+    <section>
+      {tours.map((tour) => (
+        <Tour {...tour} key={tour.id} removeTour={removeTour} />
+      ))}
+    </section>
+  );
 };
 
 export default Tours;
