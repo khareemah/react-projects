1b0b0d131f346936df606c8979bc1657a374870a
kareemah <ajimobikareemah@gmail.com>
2022-02-07 17:53:30 +0100

    slider setup done

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -5,6 +5,30 @@ import data from './data';
 function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    if (index < 0) {
+      let newIndex = people.length - 1;
+      setIndex(newIndex);
+    }
+    if (index > people.length - 1) {
+      let newIndex = 0;
+      setIndex(newIndex);
+    }
+  }, [index, people]);
+
+  useEffect(() => {
+    const timeout = setInterval(() => {
+      setIndex((oldValue) => {
+        let newValue = oldValue + 1;
+        return newValue;
+      });
+    }, 3000);
+
+    return () => {
+      clearInterval(timeout);
+    };
+  }, [index]);
   return (
     <section className="section">
       <div className="title">
@@ -23,12 +47,14 @@ function App() {
           if (personIndex === index) {
             position = 'activeSlide';
           }
+          if (
+            (index === 0 && personIndex === people.length - 1) ||
+            personIndex === index - 1
+          ) {
+            position = 'lastSlide';
+          }
           return (
-            <article
-              key={id}
-              className={position}
-              style={{ border: '2px solid red' }}
-            >
+            <article key={id} className={position}>
               <img src={image} alt={name} className="person-img" />
               <h4>{name}</h4>
               <p className="title">{title}</p>
@@ -37,10 +63,10 @@ function App() {
             </article>
           );
         })}
-        <button className="prev">
+        <button className="prev" onClick={() => setIndex(index - 1)}>
           <FiChevronLeft />
         </button>
-        <button className="next">
+        <button className="next" onClick={() => setIndex(index + 1)}>
           <FiChevronRight />
         </button>
       </div>
