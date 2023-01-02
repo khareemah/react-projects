3d219f0df1b4c3ce8f69288f9567682a008c6d3b
kareemah <ajimobikareemah@gmail.com>
2023-01-02 10:14:07 +0100

    darkmode

--- 20-dark-mode/setup/src/App.js
+++ 20-dark-mode/setup/src/App.js
@@ -1,9 +1,51 @@
-import React, { useState, useEffect } from 'react'
-import data from './data'
-import Article from './Article'
+import React, { useState, useEffect } from "react";
+import data from "./data";
 
 function App() {
-  return <h2>dark mode starter</h2>
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") || "dark-theme"
+  );
+  const toggleTheme = () => {
+    if (theme === "dark-theme") {
+      setTheme("light-theme");
+    } else {
+      setTheme("dark-theme");
+    }
+  };
+  useEffect(() => {
+    // if (!theme) {
+    //   setTheme("dark-theme");
+    // }
+    document.body.className = theme;
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+  return (
+    <main>
+      <nav>
+        <div className="nav-center">
+          <h1>overreacted</h1>
+          <button className="btn" onClick={toggleTheme}>
+            toggle
+          </button>
+        </div>
+      </nav>
+      <section className="articles">
+        {data.map((item, index) => {
+          const { title, snippet, date, length } = item;
+          return (
+            <article className="post" key={index}>
+              <h2>{title}</h2>
+              <div className="post-info">
+                <span>{moment(date).format("dddd Do, YYYY")}</span>
+                <span>{length} min read</span>
+              </div>
+              <p>{snippet}</p>
+            </article>
+          );
+        })}
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
